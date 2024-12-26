0e14aea0db461546c812a8b6cd9e31155fecd00b
Jonathan Hahn <jon.hahn@posteo.de>
2024-12-26 21:17:16 +0100

    pico_measure: add csv writer on host

--- pico_measure_host/src/main.rs
+++ pico_measure_host/src/main.rs
@@ -1,8 +1,12 @@
-use std::fmt;
+use chrono::{Datelike, Local, Timelike};
+use csv::Writer;
+use std::fmt::{self, Debug};
+use std::fs::File;
+use std::io::Read;
+use std::thread::sleep;
 use std::time::Duration;
-use std::io::{self, Read};
 
-use pico_measure_transport::{Measurement, unpack};
+use pico_measure_transport::{unpack, MeasuredValue, Measurement, MeasurementGroup};
 
 struct PicoTransErr(pico_measure_transport::Error);
 
@@ -10,48 +14,122 @@ impl fmt::Display for PicoTransErr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.0 {
             pico_measure_transport::Error::InvalidInput(_) => write!(f, "Invalid input"),
-            pico_measure_transport::Error::TooFewBufferElements => write!(f, "Too few buffer elements"),
+            pico_measure_transport::Error::TooFewBufferElements => {
+                write!(f, "Too few buffer elements")
+            }
         }
     }
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Open the USB serial port (change as needed for your OS)
-    let port_name = "/dev/ttyACM0";  // Replace with your serial port
+    let port_name = "/dev/ttyACM0"; // Replace with your serial port
     let baudrate = 115200;
+
+    loop {
+        let now = Local::now();
+
+        let logpath = format!(
+            "{}{:02}{:02}_{:02}-{:02}_pico_measure.csv",
+            now.year(),
+            now.month(),
+            now.day(),
+            now.hour(),
+            now.minute()
+        );
+
+        match record_measurements(port_name, baudrate, &logpath) {
+            Ok(v) => {}
+            Err(e) => break (Err(e)),
+        }
+    }
+}
+
+fn record_measurements(
+    port_name: &str,
+    baudrate: u32,
+    logpath: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let csv_file = File::create(logpath)?;
+
+    let mut wtr = Writer::from_writer(csv_file);
 
     let mut port = serialport::new(port_name, baudrate)
         .timeout(Duration::from_millis(700))
         .open()?;
 
-    let mut buffer = [0u8; 128];
+    let mut buffer = [0u8; 256];
     let mut buffer_cursor = 0usize;
+
+    let mut serial_errors = 0u16;
 
     println!("Initialization complete");
 
-    loop {
-        
-        match port.read(&mut buffer[buffer_cursor..]) {
-            Ok(n) => {
-                buffer_cursor += n;
-                if buffer_cursor >= buffer.len() {
-                    buffer_cursor = 0usize;
-                }
-                println!("was reading {} bytes, we have {}", n, buffer_cursor);
-            },
-            Err(e) => {
-                eprintln!("Error reading from serial port: {}", e);
-                continue;
+    while serial_errors <= 50 {
+        if let Ok(bytes_read) = port.read(&mut buffer[buffer_cursor..]) {
+            buffer_cursor += bytes_read;
+
+            if buffer_cursor >= buffer.len() {
+                buffer_cursor = 0usize;
             }
+        } else {
+            eprintln!("Error reading from serial port");
+            serial_errors += 1;
         }
 
         match unpack(&buffer[0..buffer_cursor]) {
             Ok(measurement) => {
-                println!("Received Measurement ({}): {:?}", buffer_cursor, measurement);
+                println!(
+                    "Received Measurement ({}): {:?}",
+                    buffer_cursor, measurement
+                );
+
+                write_csv_row(&mut wtr, &measurement)?;
+
+                // reset receive buffer
                 buffer_cursor = 0;
-            },
-            Err(pico_measure_transport::Error::InvalidInput(_)) => buffer_cursor = 0,
-            _ => {}
+            }
+            Err(pico_measure_transport::Error::TooFewBufferElements) => {
+                // we still hope for valid data
+            }
+            Err(_) => {
+                // give up
+                buffer_cursor = 0;
+
+                // increase error counter
+                serial_errors += 1;
+            }
         }
     }
+
+    Ok(())
+}
+
+fn write_csv_row(
+    writer: &mut csv::Writer<File>,
+    mgroup: &MeasurementGroup,
+) -> Result<(), Box<dyn std::error::Error>> {
+
+    writer.write_field(mgroup.millis.to_string())?;
+
+    let measurements: Vec<&Measurement> = mgroup
+        .measurements
+        .iter()
+        .filter_map(|x| x.as_ref())
+        .collect();
+
+    for measurement in measurements {
+        match measurement.value {
+            MeasuredValue::Volt(v) => writer.write_field(v.to_string())?,
+            MeasuredValue::Ampere(i) => writer.write_field(i.to_string())?,
+            MeasuredValue::Counts(c) => writer.write_field(c.to_string())?,
+            MeasuredValue::Celsius(t) => writer.write_field(t.to_string())?,
+        }
+    }
+
+    writer.write_record(None::<&[u8]>)?;
+
+    writer.flush()?;
+
+    Ok(())
 }
